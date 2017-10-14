b6802989c3a35d8557e164dafad965e60325d08a
Andy Stewart <lazycat.manatee@gmail.com>
2017-10-14 20:59:27 +0800

    Disable initial-scratch-message when start.

--- site-lisp/config/init-generic.el
+++ site-lisp/config/init-generic.el
@@ -93,6 +93,7 @@
 (fset 'yes-or-no-p 'y-or-n-p)           ;以 y/n代表 yes/no
 (blink-cursor-mode -1)                  ;指针不闪动
 (transient-mark-mode 1)                 ;标记高亮
+(setq initial-scratch-message "")       ;关闭启动空白buffer, 这个buffer会干扰session恢复
 (setq-default comment-style 'indent)    ;设定自动缩进的注释风格
 (setq ring-bell-function 'ignore)       ;关闭烦人的出错时的提示声
 (setq default-major-mode 'text-mode)    ;设置默认地主模式为TEXT模式
