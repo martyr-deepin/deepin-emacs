244718cd4155f0ed9d5b04354d212978196a68f7
Andy Stewart <lazycat.manatee@gmail.com>
2017-11-06 18:39:47 +0800

    Don't add dict after exit predictive module.

--- site-lisp/config/init-predictive.el
+++ site-lisp/config/init-predictive.el
@@ -79,7 +79,6 @@
 
 ;;; ### Predictive ###
 ;;; --- 英语助手
-(set-default 'predictive-auto-add-to-dict t) ;自动加入词典
 (setq predictive-dict-autosave nil)          ;不要自动保存
 (setq predictive-add-to-dict-ask nil)        ;加入词典不询问
 (setq predictive-auto-learn t)               ;自动学习
